60cbe35871dd32cdbfc07d5b07ed6bf18e46b07b
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2025-05-25 11:56:21 +0000

    Remove bootsnap

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,6 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 
-ENV['BOOTSNAP_CACHE_DIR'] ||= File.expand_path('tmp/cache/bootsnap', __dir__)
+# ENV['BOOTSNAP_CACHE_DIR'] ||= File.expand_path('tmp/cache/bootsnap', __dir__)
